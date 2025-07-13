757e040dc730beac3309bc206fad2a9a7869543d
Anastasiia Tabachnikova <nastiatab11@gmail.com>
2025-07-13 17:31:20 +0200

    Add styles for login page

--- App.tsx
+++ App.tsx
@@ -1,42 +1,10 @@
-import {
-  SafeAreaView,
-  Text,
-  TextInput,
-  TouchableOpacity,
-  View,
-} from 'react-native';
+import { SafeAreaView } from 'react-native';
+import LoginPage from './src/screen/Auth/Login';
 
 function App() {
   return (
     <SafeAreaView>
-      <View>
-        <Text>Раді тебе вітати!</Text>
-        <Text>
-          Кожен пухнастик заслоговує на дбайливих господарів. Ми допоможемо
-          знайти тобі друга
-        </Text>
-      </View>
-      <View>
-        <TouchableOpacity>
-          <Text>Вхід</Text>
-        </TouchableOpacity>
-        <TouchableOpacity>
-          <Text>Регістрація</Text>
-        </TouchableOpacity>
-      </View>
-      <View>
-        <View>
-          <View>
-            <TextInput />
-          </View>
-          <View>
-            <TextInput />
-          </View>
-        </View>
-        <TouchableOpacity>
-          <Text>Увійти</Text>
-        </TouchableOpacity>
-      </View>
+      <LoginPage />
     </SafeAreaView>
   );
 }
